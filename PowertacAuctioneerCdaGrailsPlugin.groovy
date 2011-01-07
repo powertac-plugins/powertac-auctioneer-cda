bbd0c2a5ca15d035142d05be96b36ee1eca21b06
Carsten Block <mail@carstenblock.org>
2011-01-07 17:52:12 +0100

    define powertac-common dependency as precisely 0.2.3 (and not as "greater than 0.2")

--- PowertacAuctioneerCdaGrailsPlugin.groovy
+++ PowertacAuctioneerCdaGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class PowertacAuctioneerCdaGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.6 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['powertacCommon':'0.2.3 > *']
+    def dependsOn = ['powertacCommon':'0.2.3']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
