3e22179c9fb00fd99cf3cb5f482e8acd095dd7f9
chrysopoulos18 <Anty@.(none)>
2011-03-16 19:55:09 +0200

    #127 Grails Upgrade

--- grails-app/services/org/powertac/auctioneer/cda/AuctionService.groovy
+++ grails-app/services/org/powertac/auctioneer/cda/AuctionService.groovy
@@ -3,16 +3,13 @@ package org.powertac.auctioneer.cda
 import org.powertac.common.Broker
 import org.powertac.common.IdGenerator
 import org.powertac.common.Shout
-import org.powertac.common.command.ShoutDoCreateCmd
-import org.powertac.common.command.ShoutDoDeleteCmd
-import org.powertac.common.command.ShoutDoUpdateCmd
 import org.powertac.common.exceptions.ShoutCreationException
 import org.powertac.common.interfaces.Auctioneer
 
 class AuctionService implements Auctioneer {
 
   static transactional = false
-
+/* TODO change code for new common plugin
   List processShoutCreate(ShoutDoCreateCmd shoutDoCreateCmd) throws ShoutCreationException {
     def broker = Broker.findByUserName(shoutDoCreateCmd.userName)
     Shout shoutInstance = new Shout(shoutDoCreateCmd.properties)
@@ -32,9 +29,12 @@ class AuctionService implements Auctioneer {
 
   List processShoutUpdate(ShoutDoUpdateCmd shoutDoUpdateCmd) {
     return null  //To change body of implemented methods use File | Settings | File Templates.
-  }
+  }*/  
 
-  List clearMarket() {
+  public void processShout(Shout shout){return};
+  
+  
+  void clearMarket() {
     return null  //To change body of implemented methods use File | Settings | File Templates.
   }
 }
